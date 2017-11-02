17c5ab83ddf8b9e920545c460d3f698bcf85286d
Daniel Resoalbe <resoalbe@gmail.com>
2017-11-02 01:47:55 -0400

    Dando estilo Login
    Le doy estilo al login (cambiar fondo y color de boton)

--- view/Index/Autentificacion.php
+++ view/Index/Autentificacion.php
@@ -1,43 +1,44 @@
 <!DOCTYPE html>
 <html>
-<head charset="utf-8">
-  <title>Login</title>
-</head>
+<head>
+<title>Login</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-      <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximun-scale=1.0, minimum-scale=1.0"> 
-       <link rel="stylesheet" type="text/css" href="view/Index/bootstrap/css/bootstrap.min.css">  
-       <link rel="stylesheet" type="text/css" href="view/Index/css/estilos.css" /> 
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+
        <script type="text/javascript" src="view/jquery/jquery-3.2.1.min.js"></script> 
        <script src="view/Index/js/AutentificarAjax.js"></script> 
+       <link href="view/Index/css/style-login.css" rel="stylesheet" type="text/css" media="all" />
 
+<!-- Funte WEB Roboto  --> 
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700,700i" rel="stylesheet">
+<!-- //web font --> 
+</head>
 <body>
+  <!-- main -->
+  <div class="main">
+    <div class="mt-5 main-w3lsrow">
+      <!-- login form -->
+      <div class=" login-form login-form-left"> 
+        <div class="agile-row" >
+          <h2>Ingreso al Sistema</h2> 
+          <div class="login-agileits-top">  
+            <form id = "autentificacion"  name="contact_form" method="POST"> 
+              <p>Usuario</p>
+              <input type=text name="usuario" class="name"  placeholder="Ingrese un usuario" required=""/>
+              <br>
 
+              <p >Contraseña</p>
+              <input type=password name="contrasenia"  class="password" placeholder="Ingrese una contraseña valida" required=""/>     
+              <input type="submit" value="Ingresar"> 
+            </form>   
+          </div> 
 
-
-  <div class="container" aling = center>
-    <FORM id = "autentificacion" class="contact_form" name="contact_form" method="POST">
-        <h1>Ingresar el Usuario</h1> <br>
-        <TABLE BORDER=0>
-            <TR>
-              <TD>Usuario:</TD>
-              <TD><INPUT type=text name="usuario" class="form-control" title="Se requiere un usuario valido" placeholder="Ingrese un usuario"  required/>  </TD>
-            </TR>
-            <TR>
-              <TD>Contaseña:</TD>
-              <TD><INPUT type=password name="contrasenia" class="form-control" title="Ingrese una contraseña" placeholder="Ingrese una contraseña valida"  required/>  </TD>
-          </TR>
-          <TR>
-              <TD>
-               <input type="hidden" name="datos" value="1">
-              <button class="btn btn-primary btn" type="submit">Ingresar</button>
-              <button class="btn btn-danger btn" type="reset">Cancelar</button>
-            </TD>
-          </TR>
-        </TABLE>
-    </FORM>
-</div>
-
+        </div>  
+      </div>  
+    </div>  
+  </div>  
+  <!-- //main --> 
 </body>
-       
-</html>
-
+</html
